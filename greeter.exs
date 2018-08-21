2ba5aed2f44039dd64d037109f47f229b40a902f
Giovanni Naufal <gionaufal@gmail.com>
2018-08-21 11:46:33 -0300

    Add docs for greeter

--- greeter.exs
+++ greeter.exs
@@ -1,4 +1,27 @@
 defmodule Greeter do
+  @moduledoc """
+  Provides function `hello/2` to greet a person or a list of people
+  """
+
+  @doc """
+  Prints a hello message
+
+  ## Parameters
+
+    - names: String or list that represent the name or names
+    - language_code (optional): default "en", accepts "en" and "es"
+
+  ## Examples
+
+      iex> Greeter.hello("Gio")
+      "Hello, Gio"
+
+      iex> Greeter.hello(["Gio", "Debs"])
+      "Hello, Gio, Debs"
+
+  """
+
+  @spec hello(String.t()) :: String.t()
   def hello(names, language_code \\ "en")
 
   def hello(names, language_code) when is_list(names) do
