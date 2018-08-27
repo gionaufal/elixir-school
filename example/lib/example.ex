356b17a82f725d10c0cde7e23d7d9efc58250d0a
Giovanni Naufal <gionaufal@gmail.com>
2018-08-26 21:04:16 -0300

    Add concurrency

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -15,4 +15,31 @@ defmodule Example do
   def hello do
     :world
   end
+
+  def listen do
+    receive do
+      {:ok, "hello"} -> IO.puts("World")
+    end
+
+    listen()
+  end
+
+  def add(a, b) do
+    IO.puts(a + b)
+  end
+
+  def explode, do: exit(:kaboom)
+
+  def run do
+    {pid, ref} = spawn_monitor(Example, :explode, [])
+
+    receive do
+      {:DOWN, ref, :process, from_pid, reason} -> IO.puts("Exit reason: #{reason}")
+    end
+  end
+
+  def double(x) do
+    :timer.sleep(2000)
+    x * 2
+  end
 end
